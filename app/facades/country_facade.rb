137988bd97ca06233eed494d00745a660eeb84dd
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-12 00:07:11 -0700

    Refactor:  Split find_random_country method into 2 smaller methods

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,11 +1,14 @@
 class CountryFacade
-
-  #TODO: cache method so that we can utilize same array multiple times in same session
   def self.find_random_country
+    countries = find_all_countries
+    countries.sample
+  end
+
+  # TODO: cache method so that we can utilize same array multiple times in same session
+  def self.find_all_countries
     country_data = CountryService.get_countries
     countries = country_data.map do |country|
       country[:name][:common]
     end
-    countries.sample
   end
 end
