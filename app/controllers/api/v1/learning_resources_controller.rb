7dea8b91ffec6015b947dbf56817017e8b54a6c2
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-12 22:26:33 -0700

    Refactor: update controller for correct method calls

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::LearningResourcesController < ApplicationController 
   def index
     country = country_params[:country]
-    video = VideoFacade.find_video(country)
-    images = ImageFacade.find_images(country)
+    video = VideoFacade.find_country_video(country)
+    images = ImageFacade.find_country_images(country)
     learning = LearningResource.new(country, video, images)
     render json: LearningSerializer.new(learning)
   end
