a3fc3e5a2af8c2bede2dc0ff9483e47a18beb316
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-12 17:52:19 -0700

    Feat: Add learning route and set up expected controller set up

---
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::LearningResourcesController < ApplicationController 
+  def index
+    country = country_params[:country]
+    video = VideoFacade.find_video(country)
+    images = ImageFacade.find_images(country)
+    learning = LearningResource.new(country, video, images)
+    render json: LearningSerializer.new(learning)
+  end
+
+  private 
+
+  def country_params
+    params.permit(:country)
+  end
+end
