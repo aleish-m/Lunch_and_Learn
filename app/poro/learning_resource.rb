c2ca79fed97600d930dbe7db96f990cf2bcb0f18
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-12 22:47:30 -0700

    Feat: Add learning resource poro to combine image and video dat received from separate api's into one endpoint

---
+++ app/poro/learning_resource.rb
@@ -0,0 +1,10 @@
+class LearningResource
+
+  attr_reader :country, :video, :images
+
+  def initialize(country, video, images)
+    @country = country
+    @video = video
+    @images = images
+  end
+end
