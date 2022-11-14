077fe4782c90aee386d52b4f72a0a7e56ef02bca
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 12:17:00 -0700

    Test: complete happy path testing for end point

--- spec/requests/api/v1/tourist_request_spec.rb
+++ spec/requests/api/v1/tourist_request_spec.rb
@@ -92,5 +92,54 @@ describe 'Find Tourist Sights API' do
         expect(sights_data[1][:attributes][:place_id]).to be_a(String)
       end
     end
+
+    it 'can find tourist sights near a given countrys capital city even if only a partial name received', :vcr do
+        request_params = { country: 'zealand' }
+
+        get '/api/v1/tourist_sights', params: request_params
+
+        expect(response).to be_successful
+
+        sights_response = JSON.parse(response.body, symbolize_names: true)
+        sights_data = sights_response[:data]
+
+        expect(sights_response).to be_a(Hash)
+        expect(sights_response.count).to eq(1)
+
+        expect(sights_response).to be_a(Hash)
+        expect(sights_response.count).to eq(1)
+
+        expect(sights_data).to be_an(Array)
+        expect(sights_data).to_not be_empty
+
+        expect(sights_data[0].count).to eq(3)
+        expect(sights_data[0][:id]).to be(nil)
+        expect(sights_data[0][:type]).to eq("tourist_sight")
+        expect(sights_data[0][:attributes]).to be_a(Hash)
+        expect(sights_data[0][:attributes].count).to eq(3)
+        expect(sights_data[0][:attributes][:name]).to be_a(String)
+        expect(sights_data[0][:attributes][:address]).to be_a(String)
+        expect(sights_data[0][:attributes][:place_id]).to be_a(String)
+      end
+
+    it 'returns a empty arry if no sights are found', :vcr do
+      request_params = { country: 'fran' }
+
+      get '/api/v1/tourist_sights', params: request_params
+
+      expect(response).to be_successful
+
+      sights_response = JSON.parse(response.body, symbolize_names: true)
+      sights_data = sights_response[:data]
+
+      expect(sights_response).to be_a(Hash)
+      expect(sights_response.count).to eq(1)
+
+      expect(sights_response[:data]).to be_an(Array)
+      expect(sights_response[:data]).to be_empty
+
+      expect(sights_response).to eq({data:[ ]})
+       
+    end
   end
 end
