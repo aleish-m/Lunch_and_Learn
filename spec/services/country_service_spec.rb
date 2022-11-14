2f830eb9ac8edaf1a19d0070ef6baeea752cfd1d
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 11:20:55 -0700

    Test: add test for country service

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -28,5 +28,32 @@ RSpec.describe CountryService do
 
       end
     end
+
+    it 'can data for a country', :vcr do
+      country_data = CountryService.get_info_for_country('Latvia')
+
+      expect(country_data).to be_an(Array)
+
+      country_data.each do |country|
+        expect(country).to be_a(Hash)
+
+        expect(country).to have_key(:name)
+        expect(country[:name]).to be_a(Hash)
+        expect(country[:name]).to have_key(:common)
+        expect(country[:name][:common]).to be_a(String)
+        expect(country[:name]).to have_key(:official)
+        expect(country[:name][:official]).to be_a(String)
+
+        expect(country).to have_key(:region)
+        expect(country[:region]).to be_a(String)
+
+        expect(country).to have_key(:area)
+        expect(country[:area]).to be_a(Numeric)
+
+        expect(country).to have_key(:population)
+        expect(country[:population]).to be_a(Integer)
+
+      end
+    end
   end
 end
