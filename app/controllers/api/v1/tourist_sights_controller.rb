9cf188c8e4a05df42fc7ee63de5b9f8f24352860
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 09:45:18 -0700

    Feat: Add Expected controller interaction for tourist endpoint

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::TouristSightsController < ApplicationController 
   def index
-    
+    country = country_params[:country]
+    location = CountryFacade.find_country_location(country)
+    sights = SightsFacade.find_tourist_sights(location)
+    render json: TouristSightsSerializer(sights)
+  end
+
+  private
+
+  def country_params
+    params.permit(:country)
   end
 end
