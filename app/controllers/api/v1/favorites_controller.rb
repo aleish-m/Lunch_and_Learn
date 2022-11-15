2a1989cdc5471693252e71123ca3c3d1565c1d17
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 23:28:01 -0700

    Feat: Add functionality for second sad path test for creating a favorite

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(user_params)
+    favorite = Favorite.new(favorites_params)
     if user.present?
-      favorite = Favorite.new(favorites_params)
       user.favorites << favorite
-      render json: {success: "Favorite added successfully"}, status: :created
+      if favorite.save
+        render json: {success: "Favorite added successfully"}, status: :created
+      else
+        render json: {error: 400, message: favorite.errors.full_messages.to_sentence }, status: :bad_request
+      end
     else
       render json: {error: 404, message: 'Invalid user api key'}, status: :not_found
     end
