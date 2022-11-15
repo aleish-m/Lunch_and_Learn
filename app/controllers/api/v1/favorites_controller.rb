f1192c8f284ea0e88410167794e1ef2ac65eafff
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 23:26:49 -0700

    Feat: Add functionality for first sad pth test

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
     user = User.find_by(user_params)
-    favorite = Favorite.new(favorites_params)
-    user.favorites << favorite
-    render json: {success: "Favorite added successfully"}, status: :created
+    if user.present?
+      favorite = Favorite.new(favorites_params)
+      user.favorites << favorite
+      render json: {success: "Favorite added successfully"}, status: :created
+    else
+      render json: {error: 404, message: 'Invalid user api key'}, status: :not_found
+    end
+
   end
 
   private
