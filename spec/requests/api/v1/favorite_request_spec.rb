e0a102afbc0aba741895986641b46527cac45d08
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-15 14:12:03 -0700

    Test: Add sad path testing fo finding a users favorites

--- spec/requests/api/v1/favorite_request_spec.rb
+++ spec/requests/api/v1/favorite_request_spec.rb
@@ -6,11 +6,10 @@ describe 'Favorites Endpoints' do
       it 'a user can favorite a recipe and have it saved in teh database' do
         user = create(:user)
         request_headers = { CONTENT_TYPE: 'application/json', Accept: 'application/json' }
-        request_params = JSON.generate({ api_key: user.api_key, 
-                            country: 'new zealand',
-                            recipe_link: 'http://www.marthastewart.com/355892/linguine-new-zealand-cockles',
-                            recipe_title: 'Linguine with New Zealand Cockles recipes'
-                          })
+        request_params = JSON.generate({ api_key: user.api_key,
+                                         country: 'new zealand',
+                                         recipe_link: 'http://www.marthastewart.com/355892/linguine-new-zealand-cockles',
+                                         recipe_title: 'Linguine with New Zealand Cockles recipes' })
 
         post '/api/v1/favorites', headers: request_headers, params: request_params
 
@@ -20,9 +19,9 @@ describe 'Favorites Endpoints' do
 
         expect(favorites_response).to be_a(Hash)
         expect(favorites_response.count).to eq(1)
-        expect(favorites_response).to eq({success: "Favorite added successfully"})
+        expect(favorites_response).to eq({ success: 'Favorite added successfully' })
 
-        expect(user.favorites.first.recipe_title).to eq("Linguine with New Zealand Cockles recipes")
+        expect(user.favorites.first.recipe_title).to eq('Linguine with New Zealand Cockles recipes')
       end
     end
 
@@ -30,11 +29,10 @@ describe 'Favorites Endpoints' do
       it 'if no matching user Api key is found in the database the favorite is not saved and an error message is returned' do
         user = create(:user)
         request_headers = { CONTENT_TYPE: 'application/json', Accept: 'application/json' }
-        request_params = JSON.generate({ api_key: 'JJ-3dc0vW5if5w8PBdFqvg', 
-                            country: 'new zealand',
-                            recipe_link: 'http://www.marthastewart.com/355892/linguine-new-zealand-cockles',
-                            recipe_title: 'Linguine with New Zealand Cockles recipes'
-                          })
+        request_params = JSON.generate({ api_key: 'JJ-3dc0vW5if5w8PBdFqvg',
+                                         country: 'new zealand',
+                                         recipe_link: 'http://www.marthastewart.com/355892/linguine-new-zealand-cockles',
+                                         recipe_title: 'Linguine with New Zealand Cockles recipes' })
 
         post '/api/v1/favorites', headers: request_headers, params: request_params
 
@@ -49,7 +47,7 @@ describe 'Favorites Endpoints' do
         expect(favorites_response[:error]).to eq(404)
         expect(favorites_response).to have_key(:message)
         expect(favorites_response[:message]).to be_a(String)
-        expect(favorites_response).to eq({error: 404, message: 'Invalid user api key'})
+        expect(favorites_response).to eq({ error: 404, message: 'Invalid user api key' })
 
         expect(user.favorites.first).to eq(nil)
       end
@@ -57,10 +55,9 @@ describe 'Favorites Endpoints' do
       it 'if a piece of info for a favorite is missing favorite is not saved and an error message is returned' do
         user = create(:user)
         request_headers = { CONTENT_TYPE: 'application/json', Accept: 'application/json' }
-        request_params = JSON.generate({ api_key: user.api_key, 
-                            country: 'new zealand',
-                            recipe_title: 'Linguine with New Zealand Cockles recipes'
-                          })
+        request_params = JSON.generate({ api_key: user.api_key,
+                                         country: 'new zealand',
+                                         recipe_title: 'Linguine with New Zealand Cockles recipes' })
 
         post '/api/v1/favorites', headers: request_headers, params: request_params
 
@@ -75,7 +72,7 @@ describe 'Favorites Endpoints' do
         expect(favorites_response[:error]).to eq(400)
         expect(favorites_response).to have_key(:message)
         expect(favorites_response[:message]).to be_a(String)
-        expect(favorites_response).to eq({error: 400, message: "Recipe link can't be blank"})
+        expect(favorites_response).to eq({ error: 400, message: "Recipe link can't be blank" })
 
         expect(user.favorites.first).to eq(nil)
       end
@@ -89,7 +86,7 @@ describe 'Favorites Endpoints' do
         create_list(:favorite, 5, user: user)
         create_list(:favorite, 5)
 
-        request_params = { api_key: user.api_key}
+        request_params = { api_key: user.api_key }
 
         get '/api/v1/favorites', params: request_params
 
@@ -122,7 +119,7 @@ describe 'Favorites Endpoints' do
         create(:favorite, user: user)
         create_list(:favorite, 5)
 
-        request_params = { api_key: user.api_key}
+        request_params = { api_key: user.api_key }
 
         get '/api/v1/favorites', params: request_params
 
@@ -136,6 +133,50 @@ describe 'Favorites Endpoints' do
 
         expect(favorites_data).to be_an(Array)
         expect(favorites_data.count).to be(1)
+      end
+    end
+
+    describe 'sad path' do
+      it 'returns and empty array if user has no favorites' do
+        user = create(:user)
+        create_list(:favorite, 5)
+
+        request_params = { api_key: user.api_key }
+
+        get '/api/v1/favorites', params: request_params
+
+        expect(response).to be_successful
+
+        favorites_response = JSON.parse(response.body, symbolize_names: true)
+        favorites_data = favorites_response[:data]
+
+        expect(favorites_response).to be_a(Hash)
+        expect(favorites_response.count).to eq(1)
+
+        expect(favorites_data).to be_an(Array)
+        expect(favorites_data).to be_empty
+
+        expect(favorites_response).to eq({ data: [] })
+      end
+
+      it 'returns a 404 error  with a message if invalid api_key is received' do
+        user = create(:user)
+        request_params = { api_key: 'JJ-3dc0vW5if5w8PBdFqvg'}
+
+        get '/api/v1/favorites', params: request_params
+
+        expect(response).to have_http_status(404)
+
+        favorites_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(favorites_response).to be_a(Hash)
+        expect(favorites_response.count).to eq(2)
+
+        expect(favorites_response).to have_key(:error)
+        expect(favorites_response[:error]).to eq(404)
+        expect(favorites_response).to have_key(:message)
+        expect(favorites_response[:message]).to be_a(String)
+        expect(favorites_response).to eq({ error: 404, message: 'Invalid user api key' })
       end
     end
   end
