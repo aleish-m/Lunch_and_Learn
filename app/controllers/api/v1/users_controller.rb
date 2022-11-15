f823adda49515a33a05c635171dd1248369d5104
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 20:19:53 -0700

    Feat: Finish user controller method to create a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,14 @@
-class Api::V1::UsersController <ApplicationController
+class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
-    require "pry"; binding.pry
+    if user.save
+      render json: UserSerializer.new(user), status: :created
+    else
+      render json: { error: 400, message: user.errors.full_messages.to_sentence }, status: :bad_request
+    end
   end
 
-  private 
+  private
 
   def user_params
     params.require(:user).permit(:name, :email)
