ae12d835e94b8706b9f15ddb35493745f46f0550
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-15 12:46:02 -0700

    Feat: Add Favorites serializer method for sending favorites data

--- app/serializer/favorite_serializer.rb
+++ app/serializer/favorite_serializer.rb
@@ -1,4 +1,7 @@
 class FavoriteSerializer
+  include JSONAPI::Serializer
+  attributes :recipe_title, :recipe_link, :country, :created_at 
+  
   def self.success
     {success: "Favorite added successfully"}
   end
