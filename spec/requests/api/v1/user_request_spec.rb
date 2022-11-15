17a1123dd5485fee06c3909dc174e989cd07606f
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 20:21:23 -0700

    Test: Add sad path testing for when user email is not unique

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe 'Users Endpoints' do
   describe 'Add a User to user database' do
     describe 'happy path' do
-      it 'add a new user to the user database' do
+      it 'add a new user to the user database and returns the user object once created' do
         request_headers = {CONTENT_TYPE: "application/json", Accept: "application/json"}
         request_params = '{ "name": "Athena Dao",
                          "email": "athenadao@bestgirlever.com" }'
 
         post '/api/v1/users', headers: request_headers, params: request_params
 
-        expect(response).to be_successful
+        expect(response).to have_http_status(201)
 
         user_response = JSON.parse(response.body, symbolize_names: true)
         user_data = user_response[:data]
@@ -33,6 +33,31 @@ describe 'Users Endpoints' do
         expect(user_data[:attributes][:email]).to eq('athenadao@bestgirlever.com')
         expect(user_data[:attributes]).to have_key(:api_key)
         expect(user_data[:attributes][:api_key]).to be_a(String)
+      end
+    end
+
+    describe 'sad path' do
+      it 'add a new user to the user database and returns the user object once created' do
+        user = User.create!(name: 'Athena', email:  'athenadao@bestgirlever.com')
+
+        request_headers = {CONTENT_TYPE: "application/json", Accept: "application/json"}
+        request_params = '{ "name": "Athena Dao",
+                         "email": "athenadao@bestgirlever.com" }'
+
+        post '/api/v1/users', headers: request_headers, params: request_params
+
+        expect(response).to have_http_status(400)
+
+        user_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(user_response).to be_a(Hash)
+        expect(user_response.count).to eq(2)
+
+        expect(user_response).to have_key(:error)
+        expect(user_response[:error]).to eq(400)
+        expect(user_response).to have_key(:message)
+        expect(user_response[:message]).to be_a(String)
+        expect(user_response).to eq({error: 400, message: 'Email has already been taken'})
       end
     end
   end
