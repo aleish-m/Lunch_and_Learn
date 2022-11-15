846776f4b6afd0cf9c87c8d5b46cd8707a701450
Aleisha Mork <aleisha.mork@gmail.com>
2022-11-14 20:08:15 -0700

    Feat: Add user table and model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates_presence_of :name, :email, :api_key
+  validates_uniqueness_of :email, :api_key
+  before_validation :create_new_api_key, on: :create
+ 
+  def create_new_api_key
+    self.api_key = SecureRandom.urlsafe_base64
+  end
+end
